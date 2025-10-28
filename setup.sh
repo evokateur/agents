80810f45056538a2c87e45ffa83c9b2dc9b7f983
ウエスリー <wesley@cipolla.evokateur.net>
2025-10-27 19:56:02 -0700

    tersify echo in setup.sh

--- setup.sh
+++ setup.sh
@@ -12,10 +12,10 @@ uv tool install crewai
 echo "Installing Jupyter kernel spec for $(pwd)/.venv.."
 uv run ipython kernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=agents --display-name "Agentic Engineering"
 echo
+echo "Use the 'agents' kernel in Jupyter notebooks."
+echo
 echo "To launch Jupyter Lab in the uv environment:"
 echo " uv run --with jupyter jupyter lab"
 echo
 echo "To launch nvim in the uv environment (for molten.nvim):"
 echo " uv run nvim"
-echo
-echo "Select the 'agents' kernel when working with notebooks in the web UI or running :MoltenInit"
